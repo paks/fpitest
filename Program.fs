d825c19611c98b4075978405d7a9d3f29ef18c15
Cesar Mendoza <cesar.mendoza@hyatt.com>
2019-01-31 16:16:02 -0600

    Changed program to use F# Async

--- Program.fs
+++ Program.fs
@@ -1,25 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open System.Device.Gpio
-open System.Threading
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    let controller = new GpioController(PinNumberingScheme.Logical)
-    let pin = 17
-    let lightTime = 300
-
+let blinker pin lightTime = async {
+    use controller = new GpioController(PinNumberingScheme.Logical)
     controller.OpenPin(pin, PinMode.Output)
     try
         while true do
             controller.Write(pin, PinValue.High)
-            Thread.Sleep(lightTime)
+            do! Async.Sleep(lightTime)
             controller.Write(pin, PinValue.Low)
-            Thread.Sleep(lightTime)
+            do! Async.Sleep(lightTime)
     finally
         controller.Write(pin, PinValue.Low)
         controller.ClosePin(pin)
+}
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F# in a RaspberryPi!"
+    let pin = 17
+    let lightTime = 300
+
+    Async.RunSynchronously <| blinker pin lightTime
 
     0 // return an integer exit code
